82ed57dd40734684047ed37f904d6d1446d5d6c5
Ben Schlegel <50564736+brschlegel@users.noreply.github.com>
2021-10-16 20:41:09 -0400

    We did it joe

---
+++ Tesselations/Assets/RandomSampler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSampler : MonoBehaviour
+{
+    public int numPoints = 100;
+    public List<Vector2> points;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < numPoints; i++)
+        {
+            points.Add(new Vector2(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f)));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        for (int i = 0; i < points.Count; i++)
+        {
+
+            Vector3 pos = new Vector3(points[i].x, transform.position.y + .3f, points[i].y);
+            Gizmos.DrawCube(pos, new Vector3(.04f, .04f, .04f));
+        }
+                            
+    }
+}
